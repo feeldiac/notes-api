0ddb389577c532d81f4cb89b13b24b361dfdef2a
Fernando Díaz <feeldiac@gmail.com>
2022-10-12 06:28:59 -0500

    Config handlebars adn create the first hbs files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import express from 'express'
+import { create } from 'express-handlebars'
 import indexRoutes from './routes/index.routes'
+import path from 'path'
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'));
+
+const hbs = create({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    //One can config the partialsDir too
+    defaultLayout: "main",
+    extname: ".hbs",
+})
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+//Routes
 app.use(indexRoutes)
 
 export default app
