cd82d896c1099c6574c833d3e50ee00c0826a3e7
Fernando Díaz <feeldiac@gmail.com>
2022-10-15 10:03:15 -0500

    Created the edit note endpoint and form

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', async (req, res) => {
     const tasks = await Task.find().lean()
-    res.render('index', { tasks: tasks })
+    res.render('index', { tasks })
 })
 
 router.post('/tasks/add', async (req, res) => {
@@ -21,8 +21,22 @@ router.get('/about', (req, res) => {
     res.render('about')
 })
 
-router.get('/edit', (req, res) => {
-    res.render('edit')
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const task = await Task.findById(id).lean()
+        console.log(id);
+        res.render('edit', { task })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//Use POST as PUT
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    await Task.findByIdAndUpdate(id, req.body)
+    res.redirect('/')
 })
 
 export default router;
